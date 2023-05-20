59d4b145cb8487cb5c4dc937b5c4e8a6e907c23b
lowbrains <126882213+lowbrains@users.noreply.github.com>
2023-05-20 12:48:14 +0300

    from test to fire_bot ver2

--- tg_bot/test_tg_bot.py
+++ tg_bot/test_tg_bot.py
@@ -8,13 +8,19 @@ bot = telebot.TeleBot(API_TOKEN)
 bot.set_my_commands([
     telebot.types.BotCommand("/start", "Запуск бота")
     # telebot.types.BotCommand("/AI break", "Запуск искуственного интелекта")
-])
+    ])
 
-text_1 = '🔥 Привет! Я твой персональный защитник от пожара! 🔥 \n\nМоя цель - обеспечить безопасность и защиту твоего бизнеса и быта, предлагая широкий выбор противопожарных товаров. Что бы ты ни искал - я всегда здесь, чтобы помочь тебе. \n\nС моей помощью ты сможешь: \n🔹 Получать актуальные новости и статьи о пожарной безопасности. \n🔹 Получать рекомендации по выбору и установке противопожарных товаров. \n🔹 Оформлять заказы и получать информацию о наличии товаров. \n🔹 Получать уведомления о специальных предложениях и скидках. \n\nНе трать время на поиск информации – просто задай мне свой вопрос, и я с радостью помогу тебе в решении любых вопросов, связанных с противопожарной защитой. Доверь свою безопасность профессионалу! Со мной ты всегда будешь в надежных руках. \n\nПоехали! 🚀'
+text_main_menu = '🔥 Привет! Я твой персональный защитник от пожара! 🔥 \n\nМоя цель - обеспечить безопасность и защиту твоего бизнеса и быта, предлагая широкий выбор противопожарных товаров. Что бы ты ни искал - я всегда здесь, чтобы помочь тебе. \n\nС моей помощью ты сможешь: \n🔹 Получать актуальные новости и статьи о пожарной безопасности. \n🔹 Получать рекомендации по выбору и установке противопожарных товаров. \n🔹 Оформлять заказы и получать информацию о наличии товаров. \n🔹 Получать уведомления о специальных предложениях и скидках. \n\nНе трать время на поиск информации – просто задай мне свой вопрос, и я с радостью помогу тебе в решении любых вопросов, связанных с противопожарной защитой. Доверь свою безопасность профессионалу! Со мной ты всегда будешь в надежных руках. \n\nПоехали! 🚀'
+
+text_news = {'news1': 'wildfire', 'news2': 'laws', 'news3': 'fireman', 'news4': 'random'}
+text_order = {'order1': 'status', 'order2': 'repeat', 'order3': 'seller', 'order4': 'money_back', 'order5': 'another'}
+text_recomendation = {'rec1': 'recomendation1', 'rec2': 'recomendation2', 'rec3': 'recomendation3'}
+text_catalog = {'catalog1': 'extinguisher', 'catalog2': 'fire_cranes', 'catalog3': 'fire_inventory', 'catalog4': 'bedspreads', 'catalog5': 'fire_cabinets', 'catalog6': 'fire_shields'}
+text_sales = {'sales1': 'product_promotions', 'sales2': 'promotions_services'}
 
 def start_markup():
-    markup = types.InlineKeyboardMarkup(row_width=2) #создаем клавиатуру, выставляем 2 кнопи в строке
-    item1 = types.InlineKeyboardButton('Мой заказ', callback_data='order_question') #создаем кнопки, пишем текст, который в них будет и значение кол бека при нажатии на них
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    item1 = types.InlineKeyboardButton('Мой заказ', callback_data='order_question') 
     item2 = types.InlineKeyboardButton('Актуальные новости', callback_data='news_question')
     item3 = types.InlineKeyboardButton('Каталог продуктов', callback_data='catalog_question')
     item4 = types.InlineKeyboardButton('Рекомендации', callback_data='recomendation_question')
@@ -25,81 +31,70 @@ def start_markup():
 
 
 def news_markup():
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    item1 = types.InlineKeyboardButton('Новости про пожары', callback_data='order_option_1')
-    item2 = types.InlineKeyboardButton('Новые законы', callback_data='product_question')
-    item3 = types.InlineKeyboardButton('"В мире пожарников"', callback_data='delivery_question')
-    item4 = types.InlineKeyboardButton('Случайная новость', callback_data='money_back_question')
-    item5 = types.InlineKeyboardButton('Назад', callback_data='back_to_start')
-    markup.add(item1, item2, item3, item4, item5)
+    markup = types.InlineKeyboardMarkup(row_width=4)
+    item1 = types.InlineKeyboardButton('Новости про пожары', callback_data=text_news.get('news1'))
+    item2 = types.InlineKeyboardButton('Новые законы', callback_data=text_news.get('news2'))
+    item3 = types.InlineKeyboardButton('"В мире пожарников"', callback_data=text_news.get('news3'))
+    item4 = types.InlineKeyboardButton('Случайная новость', callback_data=text_news.get('news4'))
+    item5 = types.InlineKeyboardButton('Назад', callback_data='')
+    item6 = types.InlineKeyboardButton('На главное меню', callback_data='back_to_start')
+    markup.add(item1, item2, item3, item4, item5, item6)
     return markup
 
 
 def order_markup():
     markup = types.InlineKeyboardMarkup(row_width=2)
-    item1 = types.InlineKeyboardButton('Статус заказа', callback_data='order_option_1')
-    item2 = types.InlineKeyboardButton('Повторить заказ', callback_data='product_question')
-    item3 = types.InlineKeyboardButton('Связаться с продавцом', callback_data='delivery_question')
-    item4 = types.InlineKeyboardButton('Оформить возврат', callback_data='money_back_question')
-    item5 = types.InlineKeyboardButton('Другой вопрос', callback_data='money_back_question')
-    item6 = types.InlineKeyboardButton('Назад', callback_data='back_to_start')
-    markup.add(item1, item2, item3, item4, item5, item6)
+    item1 = types.InlineKeyboardButton('Статус заказа', callback_data=text_order.get('order1'))
+    item2 = types.InlineKeyboardButton('Повторить заказ', callback_data=text_order.get('order2'))
+    item3 = types.InlineKeyboardButton('Связаться с продавцом', callback_data=text_order.get('order3'))
+    item4 = types.InlineKeyboardButton('Оформить возврат', callback_data=text_order.get('order4'))
+    item5 = types.InlineKeyboardButton('Другой вопрос', callback_data=text_order.get('order5'))
+    item6 = types.InlineKeyboardButton('Назад', callback_data='')
+    item7 = types.InlineKeyboardButton('На главное меню', callback_data='back_to_start')
+    markup.add(item1, item2, item3, item4, item5, item6, item7)
     return markup
 
 
 
 def catalog_markup():
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    item1 = types.InlineKeyboardButton('Огнетушители', callback_data='order_question')
-    item2 = types.InlineKeyboardButton('Пожарные краны  ', callback_data='product_question')
-    item3 = types.InlineKeyboardButton('Пожарный инвентарь', callback_data='delivery_question')
-    item4 = types.InlineKeyboardButton('Покрывала', callback_data='money_back_question')
-    item5 = types.InlineKeyboardButton('Пожарные шкафы', callback_data='money_back_question')
-    item6 = types.InlineKeyboardButton('Пожарные щиты', callback_data='money_back_question')
-    item7 = types.InlineKeyboardButton('Противопожарные двери', callback_data='money_back_question')
-    item8 = types.InlineKeyboardButton('Назад', callback_data='back_to_start')
+    markup = types.InlineKeyboardMarkup(row_width=3)
+    item1 = types.InlineKeyboardButton('Огнетушители', callback_data=text_catalog.get('catalog1'))
+    item2 = types.InlineKeyboardButton('Пожарные краны  ', callback_data=text_catalog.get('catalog2'))
+    item3 = types.InlineKeyboardButton('Пожарный инвентарь', callback_data=text_catalog.get('catalog3'))
+    item4 = types.InlineKeyboardButton('Покрывала', callback_data=text_catalog.get('catalog4'))
+    item5 = types.InlineKeyboardButton('Пожарные шкафы', callback_data=text_catalog.get('catalog5'))
+    item6 = types.InlineKeyboardButton('Пожарные щиты', callback_data=text_catalog.get('catalog6'))
+    item7 = types.InlineKeyboardButton('Назад', callback_data='')
+    item8 = types.InlineKeyboardButton('На главное меню', callback_data='back_to_start')
     markup.add(item1, item2, item3, item4, item5, item6, item7, item8)
     return markup
 
 
 def recomendation_markup():
     markup = types.InlineKeyboardMarkup(row_width=3)
-    item1 = types.InlineKeyboardButton('Тут', callback_data='order_question')
-    item2 = types.InlineKeyboardButton('будут', callback_data='product_question')
-    item3 = types.InlineKeyboardButton('рекомендации.', callback_data='delivery_question')
-    item4 = types.InlineKeyboardButton('Поскольку их', callback_data='money_back_question')
-    item5 = types.InlineKeyboardButton('очень много,', callback_data='back_to_start')
-    item6 = types.InlineKeyboardButton('мы оставили', callback_data='back_to_start')
-    item7 = types.InlineKeyboardButton('данный раздел', callback_data='back_to_start')
-    item8 = types.InlineKeyboardButton('пока пустым.', callback_data='back_to_start')
-    item9 = types.InlineKeyboardButton('Назад', callback_data='back_to_start')
-    markup.add(item1, item2, item3, item4, item5, item6, item7, item8, item9)
+    item1 = types.InlineKeyboardButton('Рекомендация №1', callback_data=text_recomendation.get('rec1'))
+    item2 = types.InlineKeyboardButton('Рекомендация №2', callback_data=text_recomendation.get('rec2'))
+    item3 = types.InlineKeyboardButton('Назад', callback_data='')
+    item4 = types.InlineKeyboardButton('На главное меню', callback_data='back_to_start')
+    markup.add(item1, item2, item3, item4)
     return markup
 
 
 def sales_markup():
-    markup = types.InlineKeyboardMarkup(row_width=1)
-    item1 = types.InlineKeyboardButton('Акции по товарам', callback_data='order_question')
-    item2 = types.InlineKeyboardButton('Акции по услугам', callback_data='product_question')
-    item3 = types.InlineKeyboardButton('Назад', callback_data='back_to_start')
-    markup.add(item1, item2, item3)
+    markup = types.InlineKeyboardMarkup(row_width=3)
+    item1 = types.InlineKeyboardButton('Акции по товарам', callback_data=text_sales.get('sales1'))
+    item2 = types.InlineKeyboardButton('Акции по услугам', callback_data=text_sales.get('sales2'))
+    item3 = types.InlineKeyboardButton('Назад', callback_data='')
+    item4 = types.InlineKeyboardButton('На главное меню', callback_data='back_to_start')
+    markup.add(item1, item2, item3, item4)
     return markup
-
-
-# def option_order_1():
-#     markup = types.InlineKeyboardMarkup(row_width=1)
-#     item1 = types.InlineKeyboardButton('Опция 1 по опции 1 заказа', callback_data='order_option_option_1')
-#     item2 = types.InlineKeyboardButton('Опция 2 по опции 1 заказа', callback_data='order_option_option_2')
-#     item5 = types.InlineKeyboardButton('Назад', callback_data='back_to_order')
-#     markup.add(item1, item2,item5)
-#     return markup
 
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 
-                     text_1, 
+                     text_main_menu, 
                      reply_markup=start_markup())
 
 @bot.callback_query_handler(func=lambda call:True)
@@ -125,20 +120,15 @@ def callback_start(call):
                                   message_id=call.message.id, 
                                   text= 'О каких методах правильной защиты вы бы хотели узнать?', 
                                   reply_markup=recomendation_markup())
-        elif call.data == 'back_to_start':
-            bot.edit_message_text(chat_id=call.message.chat.id, 
-                                  message_id=call.message.id, 
-                                  text= text_1, 
-                                  reply_markup=start_markup())
         elif call.data == 'discounts_promotions_questions':
             bot.edit_message_text(chat_id=call.message.chat.id, 
                                   message_id=call.message.id, 
                                   text= 'Представляем вашему вниманию список наших акций: ', 
                                   reply_markup=sales_markup())
-        # elif call.data == 'order_option_1':
-        #     bot.edit_message_text(chat_id=call.message.chat.id, 
-        #                           message_id=call.message.id, 
-        #                           text= 'Выберите подопцию', 
-        #                           reply_markup=option_order_1())
+        elif call.data == 'back_to_start':
+            bot.edit_message_text(chat_id=call.message.chat.id, 
+                                  message_id=call.message.id, 
+                                  text= text_main_menu, 
+                                  reply_markup=start_markup())
 
 bot.infinity_polling(timeout=10, long_polling_timeout=5)
